a9768e9f0387ed7a0781785d65a58c47f2679924
anferneealviar99 <anferneealviar99@hotmail.com>
2024-09-25 12:31:49 +0800

    changed Docker code logic

--- run.bat
+++ run.bat
@@ -1,3 +1,21 @@
+@REM @echo off
+@REM set IMAGE_NAME=cropcounter
+
+@REM :: Navigate to the directory where the script is located
+@REM cd /d %~dp0
+
+@REM :: Check if the image exists
+@REM docker images -q %IMAGE_NAME% >nul 2>&1
+@REM if errorlevel 1 (
+@REM     echo Image not found. Building the Docker image...
+@REM     docker build -t %IMAGE_NAME% .
+@REM ) else (
+@REM     echo Image already exists. Skipping build.
+@REM )
+
+@REM :: Run the Docker container
+@REM docker run -p 8501:8501 %IMAGE_NAME%
+
 @echo off
 set IMAGE_NAME=cropcounter
 
@@ -5,8 +23,9 @@ set IMAGE_NAME=cropcounter
 cd /d %~dp0
 
 :: Check if the image exists
-docker images -q %IMAGE_NAME% >nul 2>&1
-if errorlevel 1 (
+FOR /F "tokens=*" %%i IN ('docker images -q %IMAGE_NAME%') DO SET IMAGE_EXISTS=%%i
+
+if "%IMAGE_EXISTS%"=="" (
     echo Image not found. Building the Docker image...
     docker build -t %IMAGE_NAME% .
 ) else (
