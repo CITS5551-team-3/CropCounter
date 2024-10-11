9f921bd0a66b5960668bcbaad723d34af0ef7988
bwx <bwx8369@gmail.com>
2024-10-11 13:25:18 +0800

    filtering contours locally

--- app/contours.py
+++ app/contours.py
@@ -30,18 +30,31 @@ def split_contour(img, contour, max_width):
             # Loop over the split regions
             x_offset = x + i * max_width
 
-            mask = np.zeros_like(img)
+            # mask = np.zeros_like(img)
             counter['mask'] += time.time() - start
-            cv2.rectangle(mask, (x_offset, y), (min(x_offset + max_width, x + w), y + h), 255, -1)
+            # cv2.rectangle(mask, (x_offset, y), (min(x_offset + max_width, x + w), y + h), 255, -1)
+            minx, miny = x_offset, y
+            maxx, maxy = min(x_offset + max_width, x + w), y + h
 
             # Find contours within this region
             start = time.time()
-            split_contours, _ = cv2.findContours(cv2.bitwise_and(mask, img), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            subimg = img[miny:maxy, minx:maxx]
+            # split_contours, _ = cv2.findContours(cv2.bitwise_and(mask, img), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            split_contours, _ = cv2.findContours(subimg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             counter['findContours'] += time.time() - start
-            
+
+            split_contours = list(split_contours)
+
+            for ci, ct in enumerate(split_contours):
+                for pi, p in enumerate(ct):
+                    ct[pi][0][0] += minx
+                    ct[pi][0][1] += miny
+                split_contours[ci] = ct
+
             # # Add the split contours to the list
             new_contours.extend(split_contours)
-            
+
             # new_x = x_offset
             # new_w = min(max_width, x + w - new_x)
             # new_contour = np.array([
